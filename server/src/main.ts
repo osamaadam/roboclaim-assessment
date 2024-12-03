7d3627bff2191743415a00041fb24f50880946b6
Osama Adam <osama.adam@saloodo.com>
2024-12-03 16:01:52 +0200

    initial commit

---
+++ server/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import winston from 'winston';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp({
+              format: 'YYYY-MM-DD HH:mm:ss',
+            }),
+            winston.format.printf(({ level, message, timestamp }) => {
+              return `${timestamp} [${level}] ${message}`;
+            }),
+            winston.format.colorize({
+              all: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
