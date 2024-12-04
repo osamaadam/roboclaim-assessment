d4b32d88cd48433b605cf56ff11436fbc8138985
Osama Adam <osama.adam@saloodo.com>
2024-12-04 23:46:11 +0200

    feat: csv parser

--- server/src/processor/processors/csv_processor.service.ts
+++ server/src/processor/processors/csv_processor.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { IFileProcessor } from './file_processor.interface';
+import { createReadStream } from 'fs';
+import { parse } from '@fast-csv/parse';
 
 @Injectable()
 export class CSVFileProcessor implements IFileProcessor {
   async process(file: Express.Multer.File) {
-    return 'yo';
+    return this.__csvToJSON(file);
+  }
+
+  private async __csvToJSON(file: Express.Multer.File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const rows: Record<string, string | number>[] = [];
+      createReadStream(file.path)
+        .pipe(parse({ headers: true }))
+        .on('error', (error) => reject(error))
+        .on('data', (row) => rows.push(row))
+        .on('end', () => resolve(JSON.stringify(rows)));
+    });
   }
 }
